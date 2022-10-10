4d9f00d66d4aa38693e3289d1b655b3560996d1d
Mateo Merino <66422394+teomerino1@users.noreply.github.com>
2022-10-10 14:27:37 -0300

    Add files via upload

---
+++ uart.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.10.2022 15:31:34
+// Design Name: 
+// Module Name: uart
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart(
+
+    );
+endmodule
